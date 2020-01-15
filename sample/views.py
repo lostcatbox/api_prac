c1624cde60ca49d3ec7f2098b7447f8d76973a7a
lostcatbox <lostcatbox@gmail.com>
2020-01-15 17:21:54 +0900

    2강 종료

--- sample/views.py
+++ sample/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import Post
 from .serializers import PostSerializer
 
@@ -7,5 +7,9 @@ from .serializers import PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+#class PostListAPIView(generics.ListAPIView):
+    #queryset = Post.objects.all()
+    #serializer_class = PostSerializer
 
 # Create your views here.
