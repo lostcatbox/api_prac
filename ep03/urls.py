2eb9c0066088d1fdf61fad18d1c28c9985c33190
lostcatbox <lostcatbox@gmail.com>
2020-01-16 17:34:35 +0900

    ViewSet router안씀

--- ep03/urls.py
+++ ep03/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('post/', views.PostListAPIView.as_view()),
     path('post/<int:pk>/', views.PostDetailAPIVIew.as_view()),
+    path('user/', views.user_list),
+    path('user/<int:pk>/', views.user_detail),
 
 ]
--- ep03/views.py
+++ ep03/views.py
@@ -1,8 +1,10 @@
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
 from rest_framework import mixins, generics
-from .serializers import PostSerializer
+from rest_framework import viewsets
+from .serializers import PostSerializer, UserSerializer
 from .models import Post
 from django.shortcuts import get_object_or_404
 
@@ -19,6 +21,17 @@ class PostDetailAPIVIew(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
 
 
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
 
+
+user_list = UserViewSet.as_view({
+    'get': 'list',  # 호출될 함수와 호출할 함수를 지정합니다.
+})
+
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve',
+})
 
 # Create your views here.
