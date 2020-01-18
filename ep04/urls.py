3815dd4cb94248c5e62d76c2538e97eb24ef355f
lostcatbox <lostcatbox@gmail.com>
2020-01-18 12:32:13 +0900

    뷰셋을 router를 통해 쉽게 url2개를 지원하는것을 만들고 각자 요청에 따라 처리도 자동으로 처리됨

--- ep04/urls.py
+++ ep04/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'post', views.PostViewSet)
 
 urlpatterns = [
-    path('post/', views.post_list),
-    path('post/<int:pk>', views.post_detail),
+    path('', include(router.urls)),
+    # path('post/', views.post_list),
+    # path('post/<int:pk>', views.post_detail),
 ]
--- ep04/views.py
+++ ep04/views.py
@@ -7,17 +7,18 @@ class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-# readonlymodelviewset이므로 get요청밖에없음
-#아래형식으로 post_list라는 뷰함수를 만든것
-post_list = PostViewSet.as_view({
-    'get': 'list',
-    'post': 'create',
-})
 
-post_detail = PostViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy',
-})
+# # readonlymodelviewset이므로 get요청밖에없음
+# #아래형식으로 post_list라는 뷰함수를 만든것
+# post_list = PostViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create',
+# })
+#
+# post_detail = PostViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy',
+# })
 # Create your views here.
