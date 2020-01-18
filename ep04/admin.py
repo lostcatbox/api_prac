f7efb6caee741ebc91f80fa061faa546a8b63990
lostcatbox <lostcatbox@gmail.com>
2020-01-18 11:44:33 +0900

    ep04 migrate

--- ep04/admin.py
+++ ep04/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin)
+class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_public', 'created_at')
 
 # Register your models here.
