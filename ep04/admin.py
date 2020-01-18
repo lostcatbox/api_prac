6c74af7e86a5cea51ccf007780d76ed915768a3e
lostcatbox <lostcatbox@gmail.com>
2020-01-18 11:43:04 +0900

    ep04 model에 is_public필드 추가 후에 ep04admin에 model추가

--- ep04/admin.py
+++ ep04/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin)
+    list_display = ('title', 'is_public', 'created_at')
 
 # Register your models here.
