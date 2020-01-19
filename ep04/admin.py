29016254330fc31ed6a2816434d752b2924749de
lostcatbox <lostcatbox@gmail.com>
2020-01-19 13:29:05 +0900

    SearchFilter를 통한 Post필터링 수행

--- ep04/admin.py
+++ ep04/admin.py
@@ -4,5 +4,6 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_public', 'created_at')
+    search_fields = ('title', 'content')
 
 # Register your models here.
--- ep06/views.py
+++ ep06/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render
 from ep04.models import Post
 from ep04.serializers import PostSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
+    filter_backends = [SearchFilter]
+    search_fields = ['title']
+'''
     def get_queryset(self):
         qs = super().get_queryset()
+
+        search = self.request.GET.get('search','')
+
+        if search:
+            qs = filter(title__icontains=search)
         if self.request.user.is_authenticated:  #로그인상태라면
             qs = qs.filter(
                 author=self.request.user
@@ -18,5 +26,5 @@ class PostViewSet(ModelViewSet):
 
 
         return qs
-
+'''
 # Create your views here.
