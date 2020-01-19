d992c31c06e027ed4718f43dcaae4c386b1de4b7
lostcatbox <lostcatbox@gmail.com>
2020-01-19 12:41:23 +0900

    ep04에 author이웃필드로 추가

--- api_prec/settings.py
+++ api_prec/settings.py
@@ -130,6 +130,6 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        #'rest_framework.renderers.BrowsableAPIRenderer'
+        'rest_framework.renderers.BrowsableAPIRenderer'
     )
 }
--- ep06/views.py
+++ ep06/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from ep04.models import Post
+from ep04.serializers import PostSerializer
+from rest_framework.viewsets import ModelViewSet
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(
+            title__icontains='두'
+        )
+        return qs
 
 # Create your views here.
