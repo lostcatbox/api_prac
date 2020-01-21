589f7db998a5f04ddc8ee0033e3ce746f4a47a45
lostcatbox <lostcatbox@gmail.com>
2020-01-21 23:24:34 +0900

    validate_title , validate 만들어봄

--- ep03/serializers.py
+++ ep03/serializers.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
 from .models import Post
 from django.contrib.auth import get_user_model
+from rest_framework.exceptions import ValidationError
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+
+    def validate_title(self, value):
+        if 'django' not in value:
+            raise ValidationError('제목에 django를 꼭 포함시켜주세요')
+        return value
+
+    def validate(self, data):
+        if len(data['title'])%2==0 or len(data['content']) % 2 == 0:
+            raise ValidationError('글자갯수를 홀수로만 입력해주세요')
+        return data
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
