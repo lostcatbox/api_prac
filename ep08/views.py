794b959a25e763fa9e10aceae839748be3f4bb9c
lostcatbox <lostcatbox@gmail.com>
2020-01-21 23:48:39 +0900

    ep08종료, ep08앱에서 admin추가 ㄷ자동으로 사용자 ip주소를 serializer.save()일어날때 같이 저장구현

--- ep08/views.py
+++ ep08/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import PostSerializer
+from rest_framework.viewsets import ModelViewSet
+from .models import Post
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(ip=self.request.META['REMOTE_ADDR'])
 
 # Create your views here.
