c927633027d6f860ccd8b72202f9e298ee6b9174
lostcatbox <lostcatbox@gmail.com>
2020-01-25 16:09:19 +0900

    pagenations 끄고 throlle 끔

--- api_prec/settings.py
+++ api_prec/settings.py
@@ -128,10 +128,3 @@ STATIC_URL = '/static/'
 
 
 
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer'),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
-}
